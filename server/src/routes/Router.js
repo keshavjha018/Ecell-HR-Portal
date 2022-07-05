52e7c249f266597d1955bb2b397adeae062de15b
Keshav <keshavjha018@gmail.com>
2022-07-05 12:42:59 +0530

    login

--- server/src/routes/Router.js
+++ server/src/routes/Router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const userRoutes = require("./user.route");
-// const authRoutes = require("./auth.route");
+const authRoutes = require("./auth.route");
 // const complaintRoutes = require("./complaint.route");
 
 //---INSTANCE------
@@ -12,7 +12,7 @@ router.get('/', async(req,res) => {
 });
 
 router.use("/api/user", userRoutes);
-// router.use("/api/auth", authRoutes);
+router.use("/api/auth", authRoutes);
 // router.use("/api/complaint", complaintRoutes);
 
 module.exports = router;
