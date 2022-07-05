5450dafea8c4a40e5a1de72dbb3d6ef51145a0d6
Keshav <keshavjha018@gmail.com>
2022-07-05 16:47:40 +0530

    mail verification (backend)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import EmailVerify from './pages/EmailVerify';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World !</h1>
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route exact path ="/" element={<Home/>} />
+        <Route exact path ="/verifymail" element={<EmailVerify/>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -4,6 +4,6 @@ const Users = require("./../controllers/user.controller");
 let userRouter = new express.Router();
 let User = new Users();
 
-userRouter.post("/signUp", async(req,res) => {User.signUp(req,res)});       //Register new User
+userRouter.post("/signup", async(req,res) => {User.signUp(req,res)});       //Register new User
 
 module.exports = userRouter;
