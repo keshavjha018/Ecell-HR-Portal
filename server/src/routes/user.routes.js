41080adaccf32ab7a9ec85065232748fa56cdedc
hemant935 <82675433+hemant935@users.noreply.github.com>
2022-07-06 16:43:19 +0530

    get user route

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -5,5 +5,6 @@ let userRouter = new express.Router();
 let User = new Users();
 
 userRouter.post("/signup", async(req,res) => {User.signUp(req,res)});       //Register new User
+userRouter.get("/:id",async(req,res)=>{User.get(req,res)});                 //get user details
 
 module.exports = userRouter;
