a21e66c9d96fc8d3acb687b34e8778f3cbe7ce00
Keshav <keshavjha018@gmail.com>
2022-07-06 15:41:03 +0530

    login+signup connected with server

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import EmailVerify from './pages/EmailVerify';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-
 function App() {
   return (
     <>
+    <ToastContainer position="top-center" autoClose="2500"/>
     <Router>
       <Routes>
         <Route exact path ="/" element={<Home/>} />
         <Route exact path ="/signup" element={<Signup/>} />
         <Route exact path ="/login" element={<Login/>} />
         <Route exact path ="/verifymail" element={<EmailVerify/>} />
+        {/* <Route exact path="*" element={<Error />} /> */}
       </Routes>
     </Router>
     </>
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./login.css";
+import {useRef,useState} from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+    const email = useRef();
+    const password = useRef();
+
+    const navigate = useNavigate();
+
+    // Submitted Email-Password
+    const handleSubmit=(e)=>{
+        e.preventDefault();         //prevents reloading page on Submit
+    }
+
+    const [user, setUser] = useState({
+        email: "",
+        password:""
+    })
+
+    const handleChange = e => {
+        const {name, value} = e.target;         //stores changes
+        setUser({ ...user, [name]:value });     //updates user
+    }
+
+    const handleLogin = async()=>{
+
+        try{
+            const res = await axios.post("http://localhost:8000/api/auth/userlogin", user);
+            console.log("Success", res);
+            navigate('/');
+            // alert(res.data.message);
+        }
+        catch(e) {
+            console.log("something went wrong");
+            // alert("Something went wrong!")
+        }
+
+    }
+
     return (
         <div className='login'>
             <div className="loginRight">
             <span className="headText">Log In to Continue...</span>
-                <form className="loginBox">
-                    <input placeholder="Email" type="email" required className="loginInput "  name="email" />
-                    <input placeholder="Password" type="password" required className="loginInput" name="password" />
-                    <button className="loginBtn">Log In</button>
+                <form className="loginBox" onSubmit={handleSubmit}>
+                    <input placeholder="Email" type="email" required className="loginInput " ref={email}  name="email" value={user.email} onChange={handleChange}/>
+                    <input placeholder="Password" type="password" required className="loginInput" ref={password} name="password" value={user.password} onChange={handleChange}/>
+                    <button className="loginBtn" onClick={handleLogin}>Log In</button>
 
                     {/* <button onClick={handleForgotPassword} className="loginForgot">Forgot Password ?</button> */}
 
